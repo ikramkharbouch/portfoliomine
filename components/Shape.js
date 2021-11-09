741c3e8550529e2c930682caaf05e1b89f760eca
ikramkharbouch <4573r14@gmail.com>
2021-11-09 12:46:25 +0100

    Add talks section

---
+++ components/Shape.js
@@ -0,0 +1,11 @@
+export const Shape = () => {
+    return (
+        <>
+        <div className="mx-auto flex gap-2 justify-between mt-10 w-2/12">
+          <div className="bg-pink-500 rounded-full shadow-lg w-28 h-3">&nbsp;</div>
+          <div className="bg-pink-500 rounded-full shadow-lg w-12 h-3">&nbsp;</div>
+          <div className="bg-pink-500 rounded-full shadow-lg w-28 h-3">&nbsp;</div>
+        </div>
+        </>
+    )
+}
